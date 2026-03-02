e21e8c914a0f669718116f8ab0d9cbd118a52e7c
Katstands <owensasher1124@gmail.com>
2026-03-02 11:31:46 -0800

    Update logincheck.js

--- logincheck.js
+++ logincheck.js
@@ -1,8 +1,9 @@
 var usern = getElementById("uname").input
+var pass = getElementById("psw").input
 
 function checkcreds(){
 
-  if(usern == "katstands"){
+  if(usern == "katstands"&&pass == "14"){
   
     window.location.replace("index.html")
     
